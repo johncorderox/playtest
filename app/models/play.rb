eb1473236d9cf356d937eadaef0960b5c500e90a
johncorderox <johncorderox>
2018-04-27 00:37:21 +0000

    missing private method

--- app/models/play.rb
+++ app/models/play.rb
@@ -1,3 +1,13 @@
 class Play < ApplicationRecord
   belongs_to :user
+
+  # Before creating a new playtest, status will be 'new'
+
+  before_create :default_status_new
+
+  private
+    def default_status_new
+      self.status = "New"
+    end
+
 end
