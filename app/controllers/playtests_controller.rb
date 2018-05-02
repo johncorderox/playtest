3eb8c5eeb32fda7aae90095ef75d4a4f8e7f74f1
John Cordero <johncorderox@gmail.com>
2018-05-01 19:54:44 -0700

    show view is rendering the info modules

--- app/controllers/playtests_controller.rb
+++ app/controllers/playtests_controller.rb
@@ -1,6 +1,13 @@
 class PlaytestsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @play = Play.all.order("id DESC")
+    if @play.blank?
+      @playtest_table_message = "No Playtests Created...."
+    else
+      @playtest_table_message = Play.count
+    end
   end
 
   def new
@@ -20,6 +27,10 @@ class PlaytestsController < ApplicationController
   end
 
   def update
+  end
+
+  def show
+    @playtest = Play.find(params[:id])
   end
 
 end
--- app/models/play.rb
+++ app/models/play.rb
@@ -8,19 +8,29 @@ class Play < ApplicationRecord
   # Access codes are used for applying to new playtests.
   before_create :new_access_code
 
+  # This function will generate a new access code
+
+  def generate_access_code
+    @new_access_code = rand(1...999999).to_s
+    @new_access_code << "TEST"
+  end
+
   private
     def default_status_new
       self.status = "Open"
     end
 
     def new_access_code
-      new_access_code = rand(1...999999).to_s
-      new_access_code << "TEST"
 
-      database_check = Play.find_by(access_code: new_access_code)
+      generate_access_code
 
-        self.access_code = new_access_code
+      database_check = Play.find_by(access_code: @new_access_code)
 
+      if database_check.blank?
+        self.access_code = @new_access_code
+      else
+        generate_access_code
+      end
     end
 
 
