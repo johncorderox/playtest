3a6d246f31cde6eae3ee502ff2d9215c4febe082
johncorderox <johncorderox@gmail.com>
2018-05-15 19:16:18 -0700

    edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_up_path_for(resource)
-    '/applications/new'
+  redirect_to playtests_path
   end
 
   def redirect_to_associated_playtest(playtest_id)
